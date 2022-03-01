da695c5f61bbcbeb82ee1bf1420996d280c62300
Lucas Avelino <lucasavelino2014@hotmail.com>
2022-02-28 22:48:29 -0300

    segundo commit

--- controller.py
+++ controller.py
@@ -1,10 +1,22 @@
 from http.client import responses
 import os
 from flask import Flask, jsonify, request, render_template
-
+from flaskext.mysql import MySQL
 
 
 app = Flask(__name__)
+
+
+# Estou configurando o banco
+
+mysql = MySQL()
+
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'bd_atividade'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 
 @app.route("/", methods=["GET"])
@@ -17,8 +29,28 @@ def retornar():
     nome = request.form["nome"]
     email = request.form["email"]
     senha = request.form["senha"]
-    
-    return str(nome)
+    if nome and email and senha:
+        conexao = mysql.connect()
+        cursor = mysql.cursor()
+        cursor.execute('INSERT INTO tb_pessoas (nome, email, senha) VALUES (%s, %s, %s)', (nome, email, senha))
+        conexao.commit()
+        cadastrado = f"O {nome} foi cadastrado com sucesso :)"
+        return cadastrado
+        #return str(nome)
+
+
+# rota de listagem dos cadastros
+
+@app.route("/listar", methods=["POST", "GET"])
+def listar_pessoas():
+    conexao = mysql.connect()
+    cursor = mysql.cursor()
+    cursor.execute('SELECT nome FROM tb_pessoas')
+    data = cursor.fetchall()
+    #for x in range(len(data)):
+        #print(data[x])
+    conexao.commit()
+    return render_template('pessoas.html', datas=data)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5002))
